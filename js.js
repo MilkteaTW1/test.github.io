ef2dd23b5d3357e28095cba8f1cff613ac69459a
MilkteaTW1 <91381955+MilkteaTW1@users.noreply.github.com>
2023-07-10 19:55:13 +0800

    wb vr test

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ if (window.screen.width>1000){
 }
 else{
     let desktop = window.document.querySelector("#desktop")
-    let mobile = window.document.querySelector("mobile")
+    let mobile = window.document.querySelector("#mobile")
     desktop.style.display = "none"
     mobile.style.display = "block"
 }
