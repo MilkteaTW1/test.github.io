d8119073dc03654d8382add8089bd55311782b21
MilkteaTW1 <91381955+MilkteaTW1@users.noreply.github.com>
2023-07-10 19:47:18 +0800

    wb ver test

---
+++ js.js
@@ -0,0 +1,12 @@
+if (window.screen.width>1000){
+    let desktop=window.document.querySelector("#desktop")
+    let mobile = window.document.querySelector("mobile")
+    desktop.style.display="block"
+    mobile.style.display="none"
+}
+else{
+    let desktop = window.document.querySelector("#desktop")
+    let mobile = window.document.querySelector("mobile")
+    desktop.style.display = "none"
+    mobile.style.display = "block"
+}
